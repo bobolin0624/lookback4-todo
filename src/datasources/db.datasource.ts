fc4324079050a286213a7a6e96e3e3de3d6dbe02
Bobo Lin <bobolincowo@gmail.com>
2025-03-16 12:03:34 +0800

    chore: add test mysql connection

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,5 +1,7 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const config = {
   name: 'db',
@@ -29,3 +31,14 @@ export class DbDataSource extends juggler.DataSource
     super(dsConfig);
   }
 }
+
+async function testMySQLConnection() {
+  try {
+    await new DbDataSource().connect();
+    console.log('✅ 成功連線到資料庫！');
+  } catch (error) {
+    console.error('❌ 無法連線到資料庫：', error);
+  }
+}
+
+testMySQLConnection()
