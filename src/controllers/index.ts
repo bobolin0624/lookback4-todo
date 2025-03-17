b2e000ad7bd5dedf2300a0de3493dd3370db6f99
bobolin <bobolincowo@gmail.com>
2025-03-17 15:42:59 +0800

    feat: add controller and service

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,3 @@
 export * from './ping.controller';
+export * from './todo.controller';
+export * from './item.controller';
---
+++ src/services/todo.service.ts
@@ -0,0 +1,26 @@
+import {repository} from '@loopback/repository';
+import {Item, Todo} from '../models';
+import {TodoRepository} from '../repositories';
+import {ItemRepository} from '../repositories/item.repository';
+
+export class TodoService {
+  constructor(
+    @repository(TodoRepository) private todoRepository: TodoRepository,
+    @repository(ItemRepository) private itemRepository: ItemRepository,
+  ) { }
+
+  async createTodoWithItems(todoData: Todo, itemsData: Item[]): Promise<Todo> {
+    // 先建立 Todo
+    const todo = await this.todoRepository.create(todoData);
+
+    // 為 Todo 建立對應的 Items
+    for (const itemData of itemsData) {
+      await this.itemRepository.create({
+        ...itemData,
+        todoId: todo.id,
+      });
+    }
+
+    return todo;
+  }
+}
